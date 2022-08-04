f6e58d07b6458704bf60056a1f2dae6333550568
leon004 <l20120965@morelia.tecnm.mx>
2022-08-03 19:03:29 -0500

    New algorithms

--- binarySearch.py
+++ binarySearch.py
@@ -15,3 +15,26 @@ Algoritmo:
 5.- Si el dato a buscar es menor que el punto medio, derecha es igual al valor medio - 1 
 6.- Se sigue ejecutando siempre y cuando izquierda sea menor o igual a derecha
 """
+
+def busqueda_binaria(dato):
+    izquierda = 0
+    derecha = len(lista)-1
+    while izquierda <= derecha:
+        medio = (izquierda + derecha) // 2
+        if dato == lista[medio]:
+            return medio
+        elif dato < lista[medio]:
+            derecha = medio - 1
+        else:
+            izquierda = medio + 1
+    return None
+
+def buscar(dato):
+    if busqueda_binaria == None:
+        return "El dato % no se encuentra"%(dato)
+    else:
+        return "El dato %d se encuentra en el indice: %d"%(dato, busqueda_binaria(dato))
+
+lista = [5, 12, 15, 30, 50, 65, 70, 87, 88, 96]
+
+print(buscar(12))
---
+++ pila.py
@@ -0,0 +1,19 @@
+""" 
+Pila: 
+Una Pila es una lista ordenada o estructura de datos en la que el modo de acceso a sus elementos es de tipo
+'LIFO' (Last In First Out, Ultimo en entrar es el primero en salir) que permite almacenar datos.
+Para el manejo de los datos se cuenta con dos operaciones básicas: apilar (push), que coloca un objeto en la pila,
+y su operacion inversa, retirar (o desapilar, pop), que retira el ultimo elemento apilado.
+
+Operaciones:
+Crear: se crea la pila vacía. (Constructor)
+Tamaño: regresa el número de elementos de la pila (size)
+Apilar: se añade un elemento a la pila(push)
+Desapilar: Se elimina el elemento frontal de la pila. (pop)
+Cima: Devuelve el elemento que está en la cima de la pila (top o peek)
+Vacía: Devuelve true si la pila está sin elementos o falso en caso de que contenga uno (empty)
+
+Las pilas pueden ser de tamaño estatico y dinamico, se pueden implementar listas, arreglos, colecciones de datos o en 
+listas enlazadas.
+"""
+
