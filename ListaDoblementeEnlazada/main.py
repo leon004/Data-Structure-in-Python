86b80315c526b68a473bf7d0a59bcf34d3403d8a
leon004 <l20120965@morelia.tecnm.mx>
2022-08-07 13:49:52 -0500

    New algorithms

--- ListaDoblementeEnlazada/main.py
+++ ListaDoblementeEnlazada/main.py
@@ -4,8 +4,18 @@ Una lista doblemente enlazada es una lisata enlazada de nodos, donde cada nodo t
 un para de campos de enlace, uno al nodo siguiente, y al otro al anterior.
 
 Caracteristicas:
+
 1.- Recorrer la estructura en ambos sentidos, de inicio a fin y de fin a inicio
 2.- Borrado mas simple de datos
 3.- Estructura dinamica
-
 """
+from lista_doblemente_enlazada import ListaDoblementeEnlazada
+
+lista = ListaDoblementeEnlazada()
+
+lista.agregar_final(12)
+lista.agregar_final(56)
+lista.agregar_final(21)
+lista.agregar_final(10)
+lista.agregar_final(11)
+lista.recorrer_inicio()
