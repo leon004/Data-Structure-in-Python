196bd8ec862ab5c952c8272ff00cfeb9233b6b4a
leon004 <l20120965@morelia.tecnm.mx>
2022-08-10 12:01:50 -0500

    New algorithms

---
+++ ArbolBinario/binary_search_tree.py
@@ -0,0 +1,64 @@
+from ast import If
+from node import Node
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+#4:45
+    def empty(self):
+        if self.root == None:
+            return True
+        else:
+            return False
+
+    def add(self, value):
+        if self.empty():
+            self.root = Node(value=value, is_root=True)
+        else:
+            node = self.__get_place(value)
+            if value <= node.value:
+                node.left  = Node(value=value, parent=None, is_left=True)
+            else:
+                node.right = Node(value=value, parent=node, is_right=True)
+    
+    def __get_place(self, value):
+        aux = self.root
+        while aux:
+            temp = aux
+            if value <= aux.value:
+                aux = aux.left
+            else:
+                aux = aux.right
+        return temp
+
+    def show_in_order(self, node):
+        #Izquierda - raiz - derecho
+        if node:
+            self.show_in_order(node.left)
+            print(node.value)
+            self.show_in_order(node.right)
+
+    def show_pos_order(self, node):
+        #izquierda dereche raiz
+        if node:
+            self.show_pos_order(node.left)
+            self.show_pos_order(node.right)
+            print(node.value)
+
+    def show_pre_order(self, node):
+        #raiz izquierda derecha
+        if node:
+            print(node.value)
+            self.show_pre_order(node.left)
+            self.show_pre_order(node.right)
+
+    def search(self, node, value):
+        if node == None:
+            return None
+        else:
+            if node.value == value:
+                return node
+            elif value <= node.value:
+                return self.search(node.left, value)
+            else:
+                return self.search(node.right, value)
---
+++ ArbolBinario/main.py
@@ -0,0 +1,15 @@
+from binary_search_tree import BinarySearchTree
+
+tree = BinarySearchTree()
+
+tree.add(8)
+tree.add(10)
+tree.add(3)
+tree.add(14)
+tree.add(13)
+tree.add(1)
+tree.add(6)
+tree.add(4)
+tree.add(7)
+
+tree.show_pre_order(tree.root)
